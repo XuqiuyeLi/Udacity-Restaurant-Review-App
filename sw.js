d84c2c54d7f884c7899246d99db773a97163b1a9
SummerLi <xl1029@nyu.edu>
2018-12-29 11:01:05 +0800

    Feat
    Created sw.js for caching static web pages

---
+++ sw.js
@@ -0,0 +1,71 @@
+let staticCacheName = 'restaurant-cache-v1';
+let urlsToCache = [
+  './',
+  './index.html',
+  './restaurant.html',
+  './css/styles.css',
+  './js/main.js',
+  './js/restaurant_info.js',
+  './js/dbhelper.js',
+  './js/sw_registration.js',
+  './data/restaurants.json',
+  './img/1.jpg',
+  './img/2.jpg',
+  './img/3.jpg',
+  './img/4.jpg',
+  './img/5.jpg',
+  './img/6.jpg',
+  './img/7.jpg',
+  './img/8.jpg',
+  './img/9.jpg',
+  './img/10.jpg',
+];
+
+
+/**
+ * Installation of service worker
+ */
+
+self.addEventListener('install', function(event) {
+  console.log('SW is installed');
+
+  event.waitUntil(
+    caches.open(staticCacheName).then(function(cache) {
+      console.log('SW is caching');
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+/**
+ * Activation of service worker
+ */
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          return cacheName.startsWith('restaurant-') && cacheName !== staticCacheName;
+        }).map(function(cacheName) {
+          return cache.delete(cacheName);
+        })
+      ).then(function() {
+        console.log('SW is activated');
+      });
+    })
+  );
+});
+
+
+/**
+ * Fetching for offline content viewing
+ */
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    }).catch(function(err) {
+      console.log('Error: ' + err);
+    })
+  );
+});
